d060536784e703267b7b5c499ddeaf1f6c80c10b
Glitch (cs1-py-practice) <none>
2019-07-13 19:37:46 +0000

    First Commit

---
+++ src/core/components/my-components.js
@@ -0,0 +1,25 @@
+export default (()=>{
+  
+  document.addEventListener('gameStart',e=>{
+  
+     
+  
+  });
+  
+
+  
+  AFRAME.registerComponent('my-first-component', {
+    schema:{
+      color:{type:'color',default:'#fff'}
+    },
+    
+    init: function () {
+      
+      let self = this;
+         
+    }
+    
+    
+  });
+
+})()
