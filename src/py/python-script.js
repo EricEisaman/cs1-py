642a6b88357231a0bf35d6e7341c434f6004c44e
Glitch (cs1-py) <none>
2019-07-13 21:26:10 +0000

    clear out python starter script

--- src/py/python-script.js
+++ src/py/python-script.js
@@ -7,21 +7,6 @@ export default `
 <script id="mypy" type="text/python">
 
 
-from browser import window
-
-window.CS1.log('Hello from Python!')
-
-favorite_fruit = 'apple'
-window.CS1.log('My favorite fruit is the ' + favorite_fruit + '!')
-
-num1 = 3
-num2 = 5
-sum = num1 + num2
-window.CS1.log('The sum of num1 and num2 is ' + str(sum) + '!')
-
-student = { 'name': 'Mary', 'school': 'Blue High', 'grade': 9 }
-window.CS1.log(student['name'] + ' attends ' + student['school'] + '.')
-
 
 </script>
 `
